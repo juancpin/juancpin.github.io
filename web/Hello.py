32270c4274b1a919a4fd5a0fa5b4cd9b7b3865ab
juancpin <juancarlospin@icloud.com>
2024-04-15 10:49:10 +0200

    COn randoms

--- web/Hello.py
+++ web/Hello.py
@@ -1,11 +1,20 @@
+#coment
+import math
+import random
+
+lista = [4, 6 , 45, 78]
+
+print ('El nº aleatorio es', random.choice(lista))
+
+print ('El nº aleatorio es', random.randint(1,1000))
+
+
 a, b, c = 6, 4, 2
 print ('Hello Jander Klander', a, c, b*a*c) 
-print(3/4) 
 print (3/4) 
-print (a)
-print (b)
-print (c)
-
 print (a, b, c)
-
+num = math.sqrt(4)
+print (num)
+print ('Pi Vale', math.pi)
+print ('Mi nº es', math.sqrt(4))
 print (a*b)
