e5bcbc9cc674e173665206c5b6dd0efb93045b84
juancpin <juancarlospin@icloud.com>
2024-04-15 10:32:20 +0200

    Ver 3 Cambiamos nombre

---
+++ web/Hello.py
@@ -0,0 +1,11 @@
+a, b, c = 6, 4, 2
+print ('Hello Jander Klander', a, c, b*a*c) 
+print(3/4) 
+print (3/4) 
+print (a)
+print (b)
+print (c)
+
+print (a, b, c)
+
+print (a*b)
--- web/Untitled-1.py
+++
@@ -1,2 +0,0 @@
-print ("Hello Jander Klandre")
-print ("NOPES")
