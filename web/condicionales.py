dc90709848cf30d2d71787d7f25f6d34ecd67123
juancpin <juancarlospin@icloud.com>
2024-04-16 17:30:18 +0200

    condicionales evaluando ifs

---
+++ web/condicionales.py
@@ -0,0 +1,11 @@
+
+a = int(input ('Introduce num a:'))
+b = int(input ('Introduce num b:'))
+if a > b:
+    print ('A es mayor que B')
+elif a < b:
+    print ('B es mayor que A')
+else:
+    print ('A y B son iguales')
+
+print ('Estamos fuera del if')
