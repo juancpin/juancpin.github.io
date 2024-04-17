a273dfd8acb7de16c27dfc43d17572cd0ee1bd83
juancpin <juancarlospin@icloud.com>
2024-04-17 06:42:45 +0200

    con diccio

---
+++ web/diccionario/diccio.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import pickle
+from pathlib import Path
+
+d = dict()
+
+file_name = input('Nombre de archivo: ')
+
+path = Path(file_name)
+if path.is_file():
+    input_file = open(file_name, 'rb')
+    d = pickle.loda(input_file)
+    input_file.close()
+else:
+    print('No existe el archivo, se creará a continuación')
+    
+document_number = input('Introduce DNI: ')
+
+if document_number in d:
+    print('La edad de' + document_number + 'es' + str(d[document_number]))
+else:
+    age('No existe, se añade. Introduce edad tb: ')
+    if agew.isnumeric():
+        num = int(age)
+        d[documentnumber] = num
+         
+        
+    
+
+output_file = open(file_name, 'wb')
+pickle.dump(d, output_file)
+output_file.close()    
