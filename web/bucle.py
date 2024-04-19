b33dae81cf15d8e54c0ac055f6439b1d5134232a
juancpin <juancarlospin@icloud.com>
2024-04-19 16:00:35 +0200

    Arreglado

--- web/bucle.py
+++ web/bucle.py
@@ -1,10 +1,9 @@
 import math
 import random
 
-a = random.randint (1,1000000)
-
+a = random.randint(1,1000000000)
+print ('Empezamos por', a)
 while a > 3:
-    a /= 2
-    
-    print (a)
-
+    a /=2
+    #dejamos 2 decimales
+    print (round (a, 2))
