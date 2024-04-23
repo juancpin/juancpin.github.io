2c233eb7e5caa2a0cabff2d1ec7ef50c49c0f932
juancpin <juancarlospin@icloud.com>
2024-04-23 07:45:29 +0200

    Sublime Merge

--- web/logas.py
+++ web/logas.py
@@ -8,10 +8,7 @@ def f1(x):
     loga = math.log10(x)
     print ('LOG de', x ,'es', loga)
     return x
-    #x = list(zip(*x))
-    #x = zip(*x)
-    
-#f1(1)
+ 
 
 x = (1000, 10000, 10, 234, 50)
 for x in x:
